43b5367b467b8dfdcb75f63ecbf40b893f97cdd4
ImaniJune <143217819+ImaniJune@users.noreply.github.com>
2023-09-19 12:50:42 -0500

    Update GamPad_IT.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamPad_IT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamPad_IT.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+ import com.acmerobotics.dashboard.FtcDashboard;
+ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
  import com.qualcomm.robotcore.eventloop.opmode.OpMode;
  import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp()
 public class GamPad_IT extends OpMode {
-     private DcMotor motor1; 
+     private DcMotor motor1;
+    @Override
      public void init(){
+        FtcDashboard dashboard= FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry())        ;
 
      }
 
