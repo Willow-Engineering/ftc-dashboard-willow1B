445a9961f5b425feed58e5798d88a4d9875f2e3c
henryhull <104516548+Astricklin3@users.noreply.github.com>
2023-09-28 12:43:25 -0500

    Update MotorRunFast_OF.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorRunFast_OF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorRunFast_OF.java
@@ -6,15 +6,19 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class MotorRunFast_OF extends OpMode {
+
     private DcMotor motor1;
+
     @Override
     public void init(){
-        FtcDashboard dashboard = FtcDashboard.getInsurance();
-        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry())
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
     }
 @Override
     public void loop(){
-        FtcDashboard dashboard = FtcDashboard.getInsurance();
-        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry())
+        if(gamepad1.a) {
+            motor1.setPower(1);
+        }
+        telemetry.update();
     }
 }
